971ca5611b3c2cd94078281fdf61b3bce09252b0
Churtified <88999504+Churtified@users.noreply.github.com>
2022-09-09 11:59:27 +0930

    initial commit

---
+++ src/homepage.js
@@ -0,0 +1,17 @@
+// const renderHome = function() {
+//     const content = document.querySelector('#content');
+
+//     const title = document.createElement("h1");
+
+//     title.classList.add('title');
+//     title.textContent = "nang kwak eatery";
+
+//     content.appendChild(title);
+
+
+//     console.log("hi");
+// }
+
+// export {
+//     renderHome,
+// };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("working");
+import './styles/styles.css';
+// import { renderHome } from "./homepage.js";
+
+console.log("working!!");
+
+// const content = document.getElementById("#content");
+
+// renderHome();
