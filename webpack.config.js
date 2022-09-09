71c74e5d0c5f332708754f1381da235987cfa088
Churtified <88999504+Churtified@users.noreply.github.com>
2022-09-09 11:59:05 +0930

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  // mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  // devtool: "inline-source-map",
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     title: 'Development',
+  //   }),
+  // ],
+  module: {
+    rules: [
+      { 
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader',],
+      },
+    ],
   },
 };
