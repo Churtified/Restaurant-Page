42ddd193cc2352606a6282f5dd104c547f0c51ef
Churtified <88999504+Churtified@users.noreply.github.com>
2022-09-09 12:13:07 +0930

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,13 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  // mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  // devtool: "inline-source-map",
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     title: 'Development',
-  //   }),
-  // ],
+  devtool: "inline-source-map",
   module: {
     rules: [
       { 
