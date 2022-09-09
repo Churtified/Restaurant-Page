39d1398b888979951cd7ba372789b8fc8c6e03b4
Churtified <88999504+Churtified@users.noreply.github.com>
2022-09-09 12:43:22 +0930

    add menu items

---
+++ src/menu.js
@@ -0,0 +1,17 @@
+const menuItems = [
+    {
+        name: "Pad Thai",
+        description: "Authentic Pad Thai on the streets of Thailand has a distinct fishy/prawny funk (which sounds thoroughly unappetising but is actually completely addictive and the very essence of true Thai street food)",
+        price: "$15",
+    },
+    {
+        name: "Massaman Curry",
+        description: "Massaman curry is a rich, flavourful, and mildly spicy Thai curry. It is a fusion dish, combining ingredients from Persia, the Indian subcontinent, and the Malay Archipelago with ingredients more commonly used in native Thai cuisine to make massaman curry paste.",
+        price: "$20",
+    },
+    {
+        name: "Som Tum",
+        description: "Zingy and spicy cold salad made with green papaya, a fruit which might be unfamiliar to most Westerners. Green papaya is simply unripened papaya, and has a juicy and slightly crunchy texture with a neutral taste.",
+        price: "$15",
+    },
+];
