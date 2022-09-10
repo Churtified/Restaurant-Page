23b06cc3caee42d7ee8fa37ce7e4d4088c59159d
Churtified <88999504+Churtified@users.noreply.github.com>
2022-09-10 11:10:28 +0930

    initial commit

---
+++ src/contact.js
@@ -0,0 +1,23 @@
+const renderContact = function() {
+    const content = document.querySelector('#content');
+
+    const main = document.createElement('main');
+    main.classList.add('main');
+
+    const title = document.createElement('h2');
+    title.classList.add('contactTitle');
+    title.textContent = "Contact Us";
+
+    const info = document.createElement('p');
+    info.classList.add('info');
+    info.textContent = "Whether you’ve made an booking and have questions about that booking, have questions about reservations or you simply have a general inquiry into our restaurant please contact us via info@nangkwokfakery.co"
+
+    main.appendChild(title);
+    main.appendChild(info);
+    
+    content.appendChild(main);
+}
+
+export {
+    renderContact,
+}
